c0f904827254aab4ecf885be0caab675d405b3df
Lucas <moonjg61@gmail.com>
2021-12-02 13:03:53 -0800

    Add files via upload

--- ts/tweet.ts
+++ ts/tweet.ts
@@ -10,13 +10,26 @@ class Tweet {
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+        //completed - where the person is Tweeting an activity they recently finished.
+        if(this.text.startsWith("Just completed") || this.text.includes("Just posted"))
+            return "completed_event";
+        //live - where the person is Tweeting that they are currently doing an activity.
+        else if(this.text.startsWith("Watch"))
+            return "live_event";
+        //achievement - where the person is indicating an achievement they have reached or a goal they have set.
+        else if(this.text.includes("Achieved"))
+            return "achievement";
+        //miscellaneous - for all RunKeeper-related discussion which did not involve posting about an activity.
+        return "miscellaneous";
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written():boolean {
         //TODO: identify whether the tweet is written
-        return false;
+        let s = this.text;
+            s = s.replace('#Runkeeper', '');
+            s = s.replace(/(https):[.[a-zA-Z0-9/-]+/, '');
+            return (s.includes("-"));
     }
 
     get writtenText():string {
@@ -24,7 +37,8 @@ class Tweet {
             return "";
         }
         //TODO: parse the written text from the tweet
-        return "";
+        let wText = this.text.substring(this.text.indexOf('-'),this.text.indexOf('https'));
+        return wText;
     }
 
     get activityType():string {
@@ -32,7 +46,16 @@ class Tweet {
             return "unknown";
         }
         //TODO: parse the activity type from the text of the tweet
-        return "";
+        //search first - numbers with dot
+        let s = this.text;
+        if(s.search(/[0-9]+[.][0-9]+/) !== -1) {
+            let idx1 = s.search(/\d/);
+            let idx2 = idx1 + s.substring(idx1).search(' ');
+            let idx3 = idx2 + s.substring(idx2 + 1).search(' ') + 1;
+            let idx4 = idx3 + s.substring(idx3 + 1).search(' ') + 1;
+            return s.substring(idx3 + 1, idx4);
+        }
+        return "unknown";
     }
 
     get distance():number {
@@ -40,11 +63,20 @@ class Tweet {
             return 0;
         }
         //TODO: prase the distance from the text of the tweet
-        return 0;
+        let d = this.text;
+        let idx1 = d.search(/\d/);
+        let idx2 = idx1 + d.substring(idx1).search(' ');
+        let idx3 = idx2 + d.substring(idx2+1).search(' ') + 1;
+        let distance = parseInt(d.substring(idx1, idx2),10);
+        //1 mi = 1.609 km
+        let dType = d.substring(idx2 + 1, idx3);
+        //change km to mi
+        return (dType === 'km' ? distance * 1.609 : distance);
     }
 
     getHTMLTableRow(rowNumber:number):string {
         //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
+        let links = this.text.substring(this.text.indexOf('https'),this.text.indexOf('#'));
         return "<tr></tr>";
     }
 }
