7c001494af870fd7f03d61e23927f2cd3489c45a
Lucas <moonjg61@gmail.com>
2021-12-02 12:56:15 -0800

    Initial commit

---
+++ js/activities.js
@@ -0,0 +1,31 @@
+function parseTweets(runkeeper_tweets) {
+	//Do not proceed if no tweets loaded
+	if(runkeeper_tweets === undefined) {
+		window.alert('No tweets returned');
+		return;
+	}
+	
+	tweet_array = runkeeper_tweets.map(function(tweet) {
+		return new Tweet(tweet.text, tweet.created_at);
+	});
+
+	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
+
+	activity_vis_spec = {
+	  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+	  "description": "A graph of the number of Tweets containing each type of activity.",
+	  "data": {
+	    "values": tweet_array
+	  }
+	  //TODO: Add mark and encoding
+	};
+	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
+
+	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
+	//Use those visualizations to answer the questions about which activities tended to be longest and when.
+}
+
+//Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function (event) {
+	loadSavedRunkeeperTweets().then(parseTweets);
+});
